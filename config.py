8bf30802495a1cf631af69080cef06391cf81d59
JaydenMcCormick <jemccormick8714@eagle.fgcu.edu>
2025-04-09 10:35:24 -0400

    change config.py to configure.py and add to gitignore

--- config.py
+++
@@ -1,6 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    sqlalchemy_string: str = "postgresql://user:passwordp@host/db" # example login, need to replace with ours
-    
-settings = Settings()
